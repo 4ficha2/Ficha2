61c96e32cdc84338c29a824b958c0d02f0b0af01
acachon <36812721+acachon@users.noreply.github.com>
2018-06-15 15:54:40 +0200

    Lista wifi
    Muestro listado wifis disponibles

--- src/pages/wifi/wifi.ts
+++ src/pages/wifi/wifi.ts
@@ -10,27 +10,62 @@ export class wifiComponent {
   [x: string]: any;
 
   private networksString: Array<string>=["Actualiza pantalla!!"];
-  private networksWifi: any;
+  private networksWifi: Array<HotspotNetwork>;
   
   constructor(private hotspot: Hotspot) { 
     
     //Uso el nuevo servicio
     //Este servicio no funciona en emulador web (ionic serve) pues requiere Cordova
     //Cordova facilita el wifi, y si corre en emulador web no va
+    
+    //Compruebo si esta activo el wifi
+    this.hotspot.isAvailable().then(
+      //ESta funcion (isAvailable) indica si existe wifi ... NO si esta activo o no
+      (flag: boolean) => 
+      {
+        if (flag) {
+          alert("Wifi activo. Bien !!");
+          //Busco el listado de redes wifi visibles y actualizo el atributo global networksString[]
+          //Llamo a esta funcion cuando he comprobado que wifi esta on para mejorar estabilidad
+          //Esta funcion activa el wifi aunque no lo estuviera previamente
+          this.hotspot.scanWifi().then(
+            (networks: Array<HotspotNetwork>) => 
+            {
+              alert("Listado redes wifi actualizado");
+              //Actualizo la variable global donde lo almaceno
+              for (let i=0; i<networks.length;i=i+1){
+                this.networksString[i]=networks[i].SSID +" - "+ networks[i].BSSID;
+              }
+            }
+          );
+
+        }else {
+          alert("No tienes un servicio de wifi !!");      
+        }
+      }  
+    );
+    
+    //alert("Estado wifi: ");
+    
+
+
+
+
+  }
+
+  private buscarWifi(): void{
+    //Busco el listado de redes wifi visibles y actualizo el atributo global networksString[]
+    //Llamo a esta funcion cuando he comprobado que wifi esta on para mejorar estabilidad
     this.hotspot.scanWifi().then(
       (networks: Array<HotspotNetwork>) => 
       {
         alert("Listado redes wifi actualizado");
-
-        this.networksString[0]=networks[0].SSID;
-        this.networksString[1]="L:"+networks.length;
-        
+        //Actualizo la variable global donde lo almaceno
         for (let i=0; i<networks.length;i=i+1){
           this.networksString[i]=networks[i].SSID +" - "+ networks[i].BSSID;
         }
       }
     );
-
   }
-
+    
 }
